a05ccb9bcb6f870c1116269e725b732d860350a8
GitHubMaxwell <maxwell.rediker@gmail.com>
2018-04-22 10:02:50 -0700

    finished calculate avg kata

---
+++ calc-avg/solution.js
@@ -0,0 +1,9 @@
+function filter_list(l) {
+  return l.filter(val => {
+    return typeof (val) !== 'string';
+  });
+}
+
+console.log(filter_list([1, 2, 'a', 'b']));
+console.log(filter_list([1, 'a', 'b', 0, 15]));
+console.log(filter_list([1, 2, 'aasf', '1', '123', 123]));
